18487cd0476cf277d6edd82142ed52dd88af39bb
Theodore Dubois <tblodt@icloud.com>
2017-11-14 16:16:02 -0800

    Create the metadata for a file when the file is created

--- fs/fake.c
+++ fs/fake.c
@@ -9,6 +9,7 @@
 #include <sys/stat.h>
 
 #include "kernel/errno.h"
+#include "kernel/process.h"
 #include "kernel/fs.h"
 
 struct ish_stat {
@@ -18,7 +19,7 @@ struct ish_stat {
     dword_t rdev;
 };
 
-static datum read_meta(struct mount *mount, const char *path, const char *type) {
+static DBM *get_db(struct mount *mount) {
     DBM *db = mount->data;
     if (db == NULL) {
         char db_path[PATH_MAX];
@@ -30,6 +31,11 @@ static datum read_meta(struct mount *mount, const char *path, const char *type) 
         assert(db != NULL);
         mount->data = db;
     }
+    return db;
+}
+
+static datum read_meta(struct mount *mount, const char *path, const char *type) {
+    DBM *db = get_db(mount);
     char keydata[MAX_PATH+strlen(type)+1];
     strcpy(keydata, type);
     strcat(keydata, ":");
@@ -45,6 +51,38 @@ static int read_ish_stat(struct mount *mount, const char *path, struct ish_stat 
     assert(d.dsize == sizeof(struct ish_stat));
     *stat = *(struct ish_stat *) d.dptr;
     return 1;
+}
+
+static int write_meta(struct mount *mount, const char *path, const char *type, datum data) {
+    DBM *db = get_db(mount);
+    char keydata[MAX_PATH+strlen(type)+1];
+    strcpy(keydata, type);
+    strcat(keydata, ":");
+    strcat(keydata, path);
+    datum key = {.dptr = keydata, .dsize = strlen(keydata)};
+    return dbm_store(db, key, data, DBM_REPLACE);
+}
+
+static int write_stat(struct mount *mount, const char *path, struct ish_stat *ishstat) {
+    datum data;
+    data.dptr = (void *) ishstat;
+    data.dsize = sizeof(struct ish_stat);
+    return write_meta(mount, path, "meta", data);
+}
+
+static struct fd *fakefs_open(struct mount *mount, char *path, int flags, int mode) {
+    struct fd *fd = realfs_open(mount, path, flags, mode);
+    if (IS_ERR(fd))
+        return fd;
+    if (flags & O_CREAT_) {
+        struct ish_stat ishstat;
+        ishstat.mode = mode | S_IFREG;
+        ishstat.uid = current->uid;
+        ishstat.gid = current->gid;
+        ishstat.rdev = 0;
+        write_stat(mount, path, &ishstat);
+    }
+    return fd;
 }
 
 static int fakefs_stat(struct mount *mount, char *path, struct statbuf *fake_stat, bool follow_links) {
@@ -93,7 +131,7 @@ ssize_t fakefs_readlink(struct mount *mount, char *path, char *buf, size_t bufsi
 }
 
 const struct fs_ops fakefs = {
-    .open = realfs_open,
+    .open = fakefs_open,
     .unlink = realfs_unlink,
     .stat = fakefs_stat,
     .access = realfs_access,
