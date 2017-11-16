6999249aa43ff743b03260507e0a74c9dd2e1ca3
Theodore Dubois <tblodt@icloud.com>
2017-11-15 17:44:30 -0800

    Delete fakefs metadata when a file is deleted

--- fs/fake.c
+++ fs/fake.c
@@ -34,17 +34,33 @@ static DBM *get_db(struct mount *mount) {
     return db;
 }
 
-static datum read_meta(struct mount *mount, const char *path, const char *type) {
-    DBM *db = get_db(mount);
-    char keydata[MAX_PATH+strlen(type)+1];
+static datum build_key(char *keydata, const char *path, const char *type) {
     strcpy(keydata, type);
     strcat(keydata, ":");
     strcat(keydata, path);
     datum key = {.dptr = keydata, .dsize = strlen(keydata)};
-    return dbm_fetch(db, key);
+    return key;
 }
 
-static int read_ish_stat(struct mount *mount, const char *path, struct ish_stat *stat) {
+static datum read_meta(struct mount *mount, const char *path, const char *type) {
+    char keydata[MAX_PATH+strlen(type)+1];
+    datum key = build_key(keydata, path, type);
+    return dbm_fetch(get_db(mount), key);
+}
+
+static void write_meta(struct mount *mount, const char *path, const char *type, datum data, int flags) {
+    char keydata[MAX_PATH+strlen(type)+1];
+    datum key = build_key(keydata, path, type);
+    dbm_store(get_db(mount), key, data, flags);
+}
+
+static int delete_meta(struct mount *mount, const char *path, const char *type) {
+    char keydata[MAX_PATH+strlen(type)+1];
+    datum key = build_key(keydata, path, type);
+    return !dbm_delete(get_db(mount), key);
+}
+
+static int read_stat(struct mount *mount, const char *path, struct ish_stat *stat) {
     datum d = read_meta(mount, path, "meta");
     if (d.dptr == NULL)
         return 0;
@@ -53,25 +69,19 @@ static int read_ish_stat(struct mount *mount, const char *path, struct ish_stat 
     return 1;
 }
 
-static int write_meta(struct mount *mount, const char *path, const char *type, datum data) {
-    DBM *db = get_db(mount);
-    char keydata[MAX_PATH+strlen(type)+1];
-    strcpy(keydata, type);
-    strcat(keydata, ":");
-    strcat(keydata, path);
-    datum key = {.dptr = keydata, .dsize = strlen(keydata)};
-    return dbm_store(db, key, data, DBM_REPLACE);
+static void write_stat(struct mount *mount, const char *path, struct ish_stat *stat, int flags) {
+    datum data;
+    data.dptr = (void *) stat;
+    data.dsize = sizeof(struct ish_stat);
+    write_meta(mount, path, "meta", data, flags);
 }
 
-static int write_stat(struct mount *mount, const char *path, struct ish_stat *ishstat) {
-    datum data;
-    data.dptr = (void *) ishstat;
-    data.dsize = sizeof(struct ish_stat);
-    return write_meta(mount, path, "meta", data);
+static int delete_stat(struct mount *mount, const char *path) {
+    return delete_meta(mount, path, "meta");
 }
 
 static struct fd *fakefs_open(struct mount *mount, char *path, int flags, int mode) {
-    struct fd *fd = realfs_open(mount, path, flags, mode);
+    struct fd *fd = realfs.open(mount, path, flags, mode);
     if (IS_ERR(fd))
         return fd;
     if (flags & O_CREAT_) {
@@ -80,16 +90,24 @@ static struct fd *fakefs_open(struct mount *mount, char *path, int flags, int mo
         ishstat.uid = current->uid;
         ishstat.gid = current->gid;
         ishstat.rdev = 0;
-        write_stat(mount, path, &ishstat);
+        write_stat(mount, path, &ishstat, DBM_INSERT);
     }
     return fd;
 }
 
+static int fakefs_unlink(struct mount *mount, char *path) {
+    int err = realfs.unlink(mount, path);
+    if (err < 0)
+        return err;
+    delete_stat(mount, path);
+    return 0;
+}
+
 static int fakefs_stat(struct mount *mount, char *path, struct statbuf *fake_stat, bool follow_links) {
     struct ish_stat ishstat;
-    if (!read_ish_stat(mount, path, &ishstat))
+    if (!read_stat(mount, path, &ishstat))
         return _ENOENT;
-    int err = realfs_stat(mount, path, fake_stat, follow_links);
+    int err = realfs.stat(mount, path, fake_stat, follow_links);
     if (err < 0)
         return err;
     fake_stat->mode = ishstat.mode;
@@ -108,17 +126,17 @@ static int fakefs_fstat(struct fd *fd, struct statbuf *fake_stat) {
     return fakefs_stat(fd->mount, path, fake_stat, false);
 }
 
-ssize_t fakefs_readlink(struct mount *mount, char *path, char *buf, size_t bufsize) {
+static ssize_t fakefs_readlink(struct mount *mount, char *path, char *buf, size_t bufsize) {
     struct ish_stat ishstat;
-    if (!read_ish_stat(mount, path, &ishstat))
+    if (!read_stat(mount, path, &ishstat))
         return _ENOENT;
     if (!S_ISLNK(ishstat.mode))
         return _EINVAL;
 
-    ssize_t err = realfs_readlink(mount, path, buf, bufsize);
+    ssize_t err = realfs.readlink(mount, path, buf, bufsize);
     if (err == _EINVAL) {
         // broken symlinks can't be included in an iOS app or else Xcode craps out
-        int fd = open(path, O_RDONLY);
+        int fd = openat(mount->root_fd, fix_path(path), O_RDONLY);
         if (fd < 0)
             return err_map(errno);
         int err = read(fd, buf, bufsize);
@@ -130,13 +148,19 @@ ssize_t fakefs_readlink(struct mount *mount, char *path, char *buf, size_t bufsi
     return err;
 }
 
+static int fakefs_mount(struct mount *mount) {
+    // TODO maybe open the database here
+    return realfs.mount(mount);
+}
+
 const struct fs_ops fakefs = {
     .open = fakefs_open,
-    .unlink = realfs_unlink,
+    .unlink = fakefs_unlink,
     .stat = fakefs_stat,
     .access = realfs_access,
     .readlink = fakefs_readlink,
     .fstat = fakefs_fstat,
-    .statfs = realfs_statfs,
     .flock = realfs_flock,
+    .statfs = realfs_statfs,
+    .mount = fakefs_mount,
 };
