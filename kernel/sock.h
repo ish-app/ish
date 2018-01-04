b50eca37eef64e0e0acbd15ac11744a46a741ffb
Theodore Dubois <tblodt@icloud.com>
2018-01-04 13:54:51 -0800

    Implement SO_KEEPALIVE

--- kernel/sock.h
+++ kernel/sock.h
@@ -2,6 +2,7 @@
 #define SYS_SOCK_H
 
 #include <sys/socket.h>
+#include <netinet/tcp.h>
 #include "kernel/errno.h"
 #include "misc.h"
 #include "debug.h"
@@ -67,10 +68,12 @@ static inline int sock_flags_to_real(int fake) {
 }
 
 #define TCP_NODELAY_ 1
+#define SO_KEEPALIVE_ 9
 
 static inline int sock_opt_to_real(int fake) {
     switch (fake) {
-        case TCP_NODELAY_: return 1;
+        case TCP_NODELAY_: return TCP_NODELAY;
+        case SO_KEEPALIVE_: return SO_KEEPALIVE;
     }
     return -1;
 }
