991673490ceb42cbbe359497bc02150258a17828
Theodore Dubois <tblodt@icloud.com>
2018-08-14 01:04:01 +0000

    Compile the vdso using clang and lld on all platforms

--- vdso/vdso.c
+++ vdso/vdso.c
@@ -1,30 +1,28 @@
-#if !__i386__ || !__linux__
-#error "VDSO must be built in Linux in 32-bit mode"
+#if !__i386__ || !__ELF__
+#error "VDSO must be built for i386 elf"
 #endif
 
-#define _GNU_SOURCE
-#include <sys/time.h>
-#include <time.h>
-#include <asm/unistd.h>
+typedef long time_t;
+typedef int clockid_t;
 
 time_t __vdso_time(time_t *t) {
     time_t result;
     __asm__("int $0x80" : "=a" (result) :
-            "0" (__NR_time), "b" (t));
+            "0" (13 /* __NR_time */), "b" (t));
     return result;
 }
 
-int __vdso_gettimeofday(struct timeval *tv, void *tz) {
+int __vdso_gettimeofday(void *timeval, void *timezone) {
     int result;
     __asm__("int $0x80" : "=a" (result) :
-            "0" (__NR_gettimeofday), "b" (tv), "c" (tz));
+            "0" (78 /* __NR_gettimeofday */), "b" (timeval), "c" (timezone));
     return result;
 }
 
-int __vdso_clock_gettime(clockid_t clock, struct timespec *ts) {
+int __vdso_clock_gettime(clockid_t clock, void *timespec) {
     int result;
     __asm__("int $0x80" : "=a" (result) :
-            "0" (__NR_clock_gettime), "b" (clock), "c" (ts));
+            "0" (265 /* __NR_clock_gettime */), "b" (clock), "c" (timespec));
     return result;
 }
 
