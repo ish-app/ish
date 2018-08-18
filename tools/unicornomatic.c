d60478a70fc7f2a370e36c4a189151755d4467b1
Theodore Dubois <tblodt@icloud.com>
2018-08-18 23:01:39 +0000

    Fix various bugs in unicornomatic

--- tools/unicornomatic.c
+++ tools/unicornomatic.c
@@ -8,6 +8,7 @@
 
 #include <unicorn/unicorn.h>
 
+#include "misc.h"
 #include "debug.h"
 #include "kernel/calls.h"
 #include "emu/interrupt.h"
@@ -29,27 +30,54 @@ void uc_trycall(uc_err res, const char *msg) {
     }
 }
 
+struct uc_regs {
+    dword_t eax;
+    dword_t ebx;
+    dword_t ecx;
+    dword_t edx;
+    dword_t esi;
+    dword_t edi;
+    dword_t ebp;
+    dword_t esp;
+    dword_t eip;
+};
+void uc_getregs(uc_engine *uc, struct uc_regs *regs) {
+    static int uc_regs_ids[] = {
+        UC_X86_REG_EAX, UC_X86_REG_EBX, UC_X86_REG_ECX, UC_X86_REG_EDX,
+        UC_X86_REG_ESI, UC_X86_REG_EDI, UC_X86_REG_EBP, UC_X86_REG_ESP,
+        UC_X86_REG_EIP,
+    };
+    void *ptrs[sizeof(uc_regs_ids)/sizeof(uc_regs_ids[0])] = {
+        &regs->eax, &regs->ebx, &regs->ecx, &regs->edx,
+        &regs->esi, &regs->edi, &regs->ebp, &regs->esp,
+        &regs->eip,
+    };
+    uc_trycall(uc_reg_read_batch(uc, uc_regs_ids, ptrs, sizeof(ptrs)/sizeof(ptrs[0])), "uc_reg_read_batch");
+}
+
 int compare_cpus(struct cpu_state *cpu, struct tlb *tlb, uc_engine *uc, int undefined_flags) {
-    dword_t uc_reg_val;
+    struct uc_regs regs;
+    uc_getregs(uc, &regs);
     collapse_flags(cpu);
+
 #define CHECK(real, fake, name) \
     if ((real) != (fake)) { \
         println(name ": real 0x%llx, fake 0x%llx", (unsigned long long) (real), (unsigned long long) (fake)); \
         debugger; \
         return -1; \
     }
-#define CHECK_REG(uc_reg, cpu_reg) \
-    uc_trycall(uc_reg_read(uc, UC_X86_REG_##uc_reg, &uc_reg_val), "uc_reg_read " #cpu_reg); \
-    CHECK(uc_reg_val, cpu->cpu_reg, #cpu_reg)
-    CHECK_REG(EAX, eax);
-    CHECK_REG(EBX, ebx);
-    CHECK_REG(ECX, ecx);
-    CHECK_REG(EDX, edx);
-    CHECK_REG(ESI, esi);
-    CHECK_REG(EDI, edi);
-    CHECK_REG(ESP, esp);
-    CHECK_REG(EBP, ebp);
-    CHECK_REG(EIP, eip);
+
+#define CHECK_REG(reg) \
+    CHECK(regs.reg, cpu->reg, #reg)
+    CHECK_REG(eip);
+    CHECK_REG(eax);
+    CHECK_REG(ebx);
+    CHECK_REG(ecx);
+    CHECK_REG(edx);
+    CHECK_REG(esi);
+    CHECK_REG(edi);
+    CHECK_REG(esp);
+    CHECK_REG(ebp);
     return 0;
 }
 
@@ -65,17 +93,34 @@ void step_tracing(struct cpu_state *cpu, struct tlb *tlb, uc_engine *uc) {
         tlb_flush(tlb);
 
     // step unicorn
-    uc_trycall(uc_emu_start(uc, cpu->eip, -1, 0, 1), "unicorn step");
+    dword_t eip;
+    uc_trycall(uc_reg_read(uc, UC_X86_REG_EIP, &eip), "unicorn read eip");
+    uc_trycall(uc_emu_start(uc, eip, -1, 0, 1), "unicorn step");
 }
 
 uc_engine *start_unicorn(struct cpu_state *cpu, struct mem *mem) {
     uc_engine *uc;
     uc_trycall(uc_open(UC_ARCH_X86, UC_MODE_32, &uc), "uc_open");
 
+    // copy registers
 #define copy_reg(cpu_reg, uc_reg) \
     uc_trycall(uc_reg_write(uc, UC_X86_REG_##uc_reg, &cpu->cpu_reg), "uc_reg_write " #cpu_reg)
     copy_reg(esp, ESP);
     copy_reg(eip, EIP);
+
+    // copy memory
+    for (page_t page = 0; page < MEM_PAGES; page++) {
+        struct pt_entry *pt = &mem->pt[page];
+        if (pt->data == NULL)
+            continue;
+        int prot = 0;
+        if (pt->flags & P_READ) prot |= UC_PROT_READ | UC_PROT_EXEC;
+        if (pt->flags & P_WRITE) prot |= UC_PROT_WRITE;
+        addr_t addr = page << PAGE_BITS;
+        void *data = pt->data->data + pt->offset;
+        uc_trycall(uc_mem_map(uc, addr, PAGE_SIZE, prot), "uc_mem_map");
+        uc_trycall(uc_mem_write(uc, addr, data, PAGE_SIZE), "uc_mem_write");
+    }
 
     return uc;
 }
@@ -107,4 +152,11 @@ int main(int argc, char *const argv[]) {
         old_cpu = *cpu;
         step_tracing(cpu, tlb, uc);
     }
+}
+
+void dump_memory(uc_engine *uc, const char *file, addr_t start, size_t size) {
+    char buf[size];
+    uc_trycall(uc_mem_read(uc, start, buf, size), "uc_mem_read");
+    FILE *f = fopen(file, "w");
+    fwrite(buf, 1, sizeof(buf), f);
 }
