bc5fc0db6f15191fcb8471ad18c0a40833cffd7f
Charlie Melbye <cmelbye@snapchat.com>
2018-11-12 18:51:45 -0800

    update about view to save settings

--- app/AboutViewController.h
+++ app/AboutViewController.h
@@ -6,11 +6,8 @@
 //
 
 #import <UIKit/UIKit.h>
-
-NS_ASSUME_NONNULL_BEGIN
+#import "UserPreferences.h"
 
 @interface AboutViewController : UITableViewController
 
 @end
-
-NS_ASSUME_NONNULL_END
--- app/AboutViewController.m
+++ app/AboutViewController.m
@@ -7,22 +7,50 @@
 
 #import "UIApplication+OpenURL.h"
 #import "AboutViewController.h"
+#import "UserPreferences.h"
 
 @interface AboutViewController ()
 @property (weak, nonatomic) IBOutlet UITableViewCell *sendFeedback;
 @property (weak, nonatomic) IBOutlet UITableViewCell *openGithub;
 @property (weak, nonatomic) IBOutlet UITableViewCell *openTwitter;
-
+@property (weak, nonatomic) IBOutlet UITableViewCell *fontSizeCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *foregroundColorCell;
+@property (weak, nonatomic) IBOutlet UITableViewCell *backgroundColorCell;
 @end
 
 @implementation AboutViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self _addObservers];
 }
 
-- (IBAction)openLink:(id)sender {
+- (void)_addObservers {
+    UserPreferences *prefs = [UserPreferences shared];
+    NSKeyValueObservingOptions opts = NSKeyValueObservingOptionInitial | NSKeyValueObservingOptionNew;
+    
+    [prefs addObserver:self forKeyPath:@"mapCapsLockAsControl" options:opts context:nil];
+    [prefs addObserver:self forKeyPath:@"fontSize" options:opts context:nil];
+    [prefs addObserver:self forKeyPath:@"foregroundColor" options:opts context:nil];
+    [prefs addObserver:self forKeyPath:@"backgroundColor" options:opts context:nil];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context {
+    if ([object isKindOfClass:[UserPreferences class]]) {
+        [self _updatePreferenceUI];
+    } else {
+        [super observeValueForKeyPath:keyPath ofObject:object change:change context:context];
+    }
+}
+
+- (void)_updatePreferenceUI {
+    UserPreferences *prefs = [UserPreferences shared];
+    _fontSizeCell.detailTextLabel.text = prefs.fontSize.stringValue;
+    _foregroundColorCell.detailTextLabel.text = prefs.foregroundColor;
+    _backgroundColorCell.detailTextLabel.text = prefs.backgroundColor;
+}
+
+- (IBAction)didSwitchCapsLock:(id)sender {
     
 }
 
@@ -34,7 +62,43 @@
         [UIApplication openURL:@"https://github.com/tbodt/ish"];
     } else if (cell == self.openTwitter) {
         [UIApplication openURL:@"https://twitter.com/tblodt"];
+    } else if (cell == self.fontSizeCell ||
+               cell == self.foregroundColorCell ||
+               cell == self.backgroundColorCell) {
+        [self _showInputForCell:cell];
     }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (void)_showInputForCell:(UITableViewCell *)cell {
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:cell.textLabel.text
+                                message:@"(Enter a CSS compatible value)"
+                                preferredStyle:UIAlertControllerStyleAlert];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = cell.detailTextLabel.text;
+    }];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Save" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        NSString *value = [[[alert textFields] firstObject] text];
+        if ([value isEqualToString:@""]) {
+            return;
+        }
+
+        UserPreferences *prefs = [UserPreferences shared];
+        
+        if (cell == self.fontSizeCell) {
+            NSUInteger fontSize = [value integerValue];
+            if (fontSize) {
+                [prefs setFontSize:@(fontSize)];
+            }
+        } else if (cell == self.foregroundColorCell) {
+            [prefs setForegroundColor:value];
+        } else if (cell == self.backgroundColorCell) {
+            [prefs setBackgroundColor:value];
+        }
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:nil]];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 @end
