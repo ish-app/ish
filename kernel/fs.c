6e741f23e4782cccf5634acb6e42f2acfb8dad1c
Theodore Dubois <tblodt@icloud.com>
2018-11-14 18:15:04 -0800

    Limit the size of write() logs

--- kernel/fs.c
+++ kernel/fs.c
@@ -189,7 +189,7 @@ dword_t sys_write(fd_t fd_no, addr_t buf_addr, dword_t size) {
     if (user_read(buf_addr, buf, size))
         return _EFAULT;
     buf[size] = '\0';
-    STRACE("write(%d, \"%s\", %d)", fd_no, buf, size);
+    STRACE("write(%d, \"%.100s\", %d)", fd_no, buf, size);
     struct fd *fd = f_get(fd_no);
     if (fd == NULL)
         return _EBADF;
