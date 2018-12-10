362935a57c1032001dbfd81dde026960801b91df
Ryan Hileman <lunixbochs@gmail.com>
2018-12-10 00:16:23 -0800

    reorganize device initialization for mem devs

---
+++ fs/mem.c
@@ -0,0 +1,53 @@
+#include "kernel/errno.h"
+#include "fs/mem.h"
+#include "fs/dev.h"
+
+// this file handles major device number 1, minor device numbers are mapped in table below
+#pragma GCC diagnostic ignored "-Winitializer-overrides"
+struct dev_ops *mem_devs[] = {
+    [0 ... 255] = NULL,
+    // [1] = &prog_mem_dev,
+    // [2] = &kmem_dev, // (not really applicable)
+    [3] = &null_dev,
+    // [4] = &port_dev,
+    // [5] = &zero_dev,
+    // [7] = &full_dev,
+    // [8] = &random_dev,
+    // [9] = &random_dev,
+    // [10] = &aio_dev,
+    // [11] = &kmsg_dev,
+    // [12] = &oldmem_dev, // replaced by /proc/vmcore
+};   
+
+// dispatch device for major device 1
+static int mem_open(int major, int minor, int type, struct fd *fd) {
+    struct dev_ops *dev = mem_devs[minor];
+    if (dev == NULL) {
+        return _ENXIO;
+    }
+    fd->ops = &dev->fd;
+    if (!dev->open)
+        return 0;
+    return dev->open(major, minor, type, fd);
+}
+
+struct dev_ops mem_dev = {
+    .open = mem_open,
+};
+
+// begin inline devices
+static int null_open(int major, int minor, int type, struct fd *fd) {
+    return 0;
+}
+static ssize_t null_read(struct fd *fd, void *buf, size_t bufsize) {
+    return 0;
+}
+static ssize_t null_write(struct fd *fd, const void *buf, size_t bufsize) {
+    return bufsize;
+}
+
+struct dev_ops null_dev = {
+    .open = null_open,
+    .fd.read = null_read,
+    .fd.write = null_write,
+};
---
+++ fs/mem.h
@@ -0,0 +1,12 @@
+#ifndef FS_NULL_H
+#define FS_NULL_H
+
+#include "kernel/fs.h"
+#include "fs/dev.h"
+
+extern struct dev_ops
+    mem_dev,
+    null_dev,
+    random_dev;
+
+#endif
