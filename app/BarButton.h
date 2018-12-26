10a04bc688d636511def99c6ecbeabb8f1936dbc
Theodore Dubois <tblodt@icloud.com>
2018-12-25 16:19:42 -0800

    Add paste button

--- app/BarButton.h
+++ app/BarButton.h
@@ -9,7 +9,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-IB_DESIGNABLE @interface BarButton : UIButton
+@interface BarButton : UIButton
 
 @property (nonatomic) UIKeyboardAppearance keyAppearance;
 @property IBInspectable BOOL secondary;
