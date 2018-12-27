467dc7f080083dcbb43c1e380b41a0178f57d99e
Theodore Dubois <tblodt@icloud.com>
2018-12-26 17:28:58 -0800

    Mount /proc automatically when running from the terminal

--- main.c
+++ main.c
@@ -13,5 +13,6 @@ int main(int argc, char *const argv[]) {
         fprintf(stderr, "%s\n", strerror(-err));
         return err;
     }
+    do_mount(&procfs, "proc", "/proc");
     cpu_run(&current->cpu);
 }
