b7ab0ddc349a48e787e8793f3b4827a4089266b9
Theodore Dubois <tblodt@icloud.com>
2019-01-31 22:15:39 -0800

    Unbreak dotdot path components

--- fs/path.c
+++ fs/path.c
@@ -46,7 +46,7 @@ int path_normalize(struct fd *at, const char *path, char *out, bool follow_links
                 continue;
             } else if (p[1] == '.' && (p[2] == '\0' || p[2] == '/')) {
                 // double dot path component, delete the last component
-                if (*o != '\0') {
+                if (o != out) {
                     do {
                         o--;
                         n++;
