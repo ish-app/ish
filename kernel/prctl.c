22b2b200d3ed8142fc9c5e67efa2fe6add02cbeb
Theodore Dubois <tblodt@icloud.com>
2019-03-24 18:17:09 -0700

    Stub PRCTL_SET_KEEPCAPS

--- kernel/prctl.c
+++ kernel/prctl.c
@@ -1,5 +1,14 @@
 #include "kernel/calls.h"
 
-int_t sys_prctl(dword_t UNUSED(option), uint_t UNUSED(arg2), uint_t UNUSED(arg3), uint_t UNUSED(arg4), uint_t UNUSED(arg5)) {
-    return _EINVAL;
+#define PRCTL_SET_KEEPCAPS_ 8
+
+int_t sys_prctl(dword_t option, uint_t UNUSED(arg2), uint_t UNUSED(arg3), uint_t UNUSED(arg4), uint_t UNUSED(arg5)) {
+    switch (option) {
+        case PRCTL_SET_KEEPCAPS_:
+            // stub
+            return 0;
+        default:
+            STRACE("prctl(%#x)", option);
+            return _EINVAL;
+    }
 }
