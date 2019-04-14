c9007940fb476b4dd68f8af5762cf1688e346617
Benjamin G <therealrandomblock1@gmail.com>
2019-04-14 13:23:52 -0500

    Update uname to reflect current version
    We're not on 3.2.0 anymore, so this should be changed to avoid confusion

--- kernel/uname.c
+++ kernel/uname.c
@@ -14,7 +14,7 @@ void do_uname(struct uname *uts) {
     memset(uts, 0, sizeof(struct uname));
     strcpy(uts->system, "Linux");
     strcpy(uts->hostname, real_uname.nodename);
-    strcpy(uts->release, "3.2.0-ish");
+    strcpy(uts->release, "3.8.0-ish");
     strcpy(uts->version, "SUPER AWESOME compiled on " __DATE__ );
     strcpy(uts->arch, "i686");
     strcpy(uts->domain, "(none)");
