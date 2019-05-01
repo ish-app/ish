a94a56cfb432e1d899bd3c50bc9176951f8035cc
Theodore Dubois <tblodt@icloud.com>
2019-05-01 16:25:29 -0700

    Rework implementation of rlimit calls
    Turns out prlimit is the only call that takes a 64-bit rlimit struct.
    The other ones are supposed to take a 32 bit struct, and I never noticed
    any stack smashing until now because musl just uses prlimit.

--- kernel/resource.c
+++ kernel/resource.c
@@ -12,71 +12,125 @@
 #include <limits.h>
 #include "kernel/calls.h"
 
-struct rlimit_ rlimit_get(struct task *task, int resource) {
-    struct tgroup *group = task->group;
-    lock(&group->lock);
-    struct rlimit_ limit = group->limits[resource];
-    unlock(&group->lock);
-    return limit;
+static bool resource_valid(int resource) {
+    return resource >= 0 && resource < RLIMIT_NLIMITS_;
 }
 
-void rlimit_set(struct task *task, int resource, struct rlimit_ limit) {
+static int rlimit_get(struct task *task, int resource, struct rlimit_ *limit) {
+    if (!resource_valid(resource))
+        return _EINVAL;
+    struct tgroup *group = task->group;
+    lock(&group->lock);
+    *limit = group->limits[resource];
+    unlock(&group->lock);
+    return 0;
+}
+
+static int rlimit_set(struct task *task, int resource, struct rlimit_ limit) {
+    if (!resource_valid(resource))
+        return _EINVAL;
     struct tgroup *group = task->group;
     lock(&group->lock);
     group->limits[resource] = limit;
     unlock(&group->lock);
+    return 0;
 }
 
 rlim_t_ rlimit(int resource) {
-    return rlimit_get(current, resource).cur;
+    struct rlimit_ limit;
+    if (rlimit_get(current, resource, &limit) != 0)
+        die("invalid resource %d", resource);
+    return limit.cur;
 }
 
-dword_t sys_getrlimit(dword_t resource, addr_t rlim_addr) {
-    struct rlimit_ rlimit = rlimit_get(current, resource);
-    STRACE("getrlimit(%d, {cur=%#x, max=%#x})", resource, rlimit.cur, rlimit.max);
+static int do_getrlimit32(int resource, struct rlimit32_ *rlimit32) {
+    STRACE("getlimit(%d)", resource);
+    struct rlimit_ rlimit;
+    int err = rlimit_get(current, resource, &rlimit);
+    if (err < 0)
+        return err;
+    STRACE(" {cur=%#x, max=%#x}", rlimit.cur, rlimit.max);
+
+    rlimit32->max = rlimit.max;
+    rlimit32->cur = rlimit.cur;
+    return 0;
+}
+
+dword_t sys_getrlimit32(dword_t resource, addr_t rlim_addr) {
+    struct rlimit32_ rlimit;
+    int err = do_getrlimit32(resource, &rlimit);
+    if (err < 0)
+        return err;
     if (user_put(rlim_addr, rlimit))
         return _EFAULT;
     return 0;
 }
 
-dword_t sys_getrlimit32(dword_t resource, addr_t rlim_addr) {
-    struct rlimit_ rlimit = rlimit_get(current, resource);
+dword_t sys_old_getrlimit32(dword_t resource, addr_t rlim_addr) {
+    struct rlimit32_ rlimit;
+    int err = do_getrlimit32(resource, &rlimit);
+    if (err < 0)
+        return err;
+
     // This version of the call is for programs that aren't aware of rlim_t
     // being 64 bit. RLIM_INFINITY looks like -1 when truncated to 32 bits.
     if (rlimit.cur > INT_MAX)
         rlimit.cur = INT_MAX;
     if (rlimit.max > INT_MAX)
         rlimit.max = INT_MAX;
-    STRACE("getrlimit32(%d, {cur=%#x, max=%#x})", resource, rlimit.cur, rlimit.max);
+
     if (user_put(rlim_addr, rlimit))
         return _EFAULT;
     return 0;
 }
 
-dword_t sys_setrlimit(dword_t resource, addr_t rlim_addr) {
+static int check_setrlimit(int resource, struct rlimit_ new_limit) {
+    if (superuser())
+        return 0;
+    struct rlimit_ old_limit;
+    int err = rlimit_get(current, resource, &old_limit);
+    if (err < 0)
+        return err;
+    if (new_limit.max > old_limit.max)
+        return _EPERM;
+    return 0;
+}
+
+dword_t sys_setrlimit32(dword_t resource, addr_t rlim_addr) {
     struct rlimit_ rlimit;
     if (user_get(rlim_addr, rlimit))
         return _EFAULT;
     STRACE("setrlimit(%d, {cur=%#x, max=%#x})", resource, rlimit.cur, rlimit.max);
-    // TODO check permissions
-    rlimit_set(current, resource, rlimit);
-    return 0;
+    int err = check_setrlimit(resource, rlimit);
+    if (err < 0)
+        return err;
+    return rlimit_set(current, resource, rlimit);
 }
 
-dword_t sys_prlimit(pid_t_ pid, dword_t resource, addr_t new_limit_addr, addr_t old_limit_addr) {
+dword_t sys_prlimit64(pid_t_ pid, dword_t resource, addr_t new_limit_addr, addr_t old_limit_addr) {
+    STRACE("prlimit64(%d, %d)", pid, resource);
     if (pid != 0)
         return _EINVAL;
 
-    int err = 0;
     if (old_limit_addr != 0) {
-        err = sys_getrlimit(resource, old_limit_addr);
+        struct rlimit_ rlimit;
+        int err = rlimit_get(current, resource, &rlimit);
         if (err < 0)
             return err;
+        STRACE(" old={cur=%#x, max=%#x}", rlimit.cur, rlimit.max);
+        if (user_put(old_limit_addr, rlimit))
+            return _EFAULT;
     }
+
     if (new_limit_addr != 0) {
-        err = sys_setrlimit(resource, new_limit_addr);
+        struct rlimit_ rlimit;
+        if (user_get(new_limit_addr, rlimit))
+            return _EFAULT;
+        STRACE(" new={cur=%#x, max=%#x}", rlimit.cur, rlimit.max);
+        int err = check_setrlimit(resource, rlimit);
         if (err < 0)
             return err;
+        return rlimit_set(current, resource, rlimit);
     }
     return 0;
 }
