9a8569ed9998ea12cf3c8fadd47ee895a1a48fc3
Theodore Dubois <tblodt@icloud.com>
2019-09-22 11:45:38 -0700

    Log the message in sendto

--- fs/sock.c
+++ fs/sock.c
@@ -542,13 +542,14 @@ close_sockets:
 }
 
 int_t sys_sendto(fd_t sock_fd, addr_t buffer_addr, dword_t len, dword_t flags, addr_t sockaddr_addr, dword_t sockaddr_len) {
-    STRACE("sendto(%d, 0x%x, %d, %d, 0x%x, %d)", sock_fd, buffer_addr, len, flags, sockaddr_addr, sockaddr_len);
     struct fd *sock = sock_getfd(sock_fd);
     if (sock == NULL)
         return _EBADF;
-    char buffer[len];
+    char buffer[len + 1];
     if (user_read(buffer_addr, buffer, len))
         return _EFAULT;
+    buffer[len] = '\0';
+    STRACE("sendto(%d, \"%.100s\", %d, %d, 0x%x, %d)", sock_fd, buffer, len, flags, sockaddr_addr, sockaddr_len);
     int real_flags = sock_flags_to_real(flags);
     if (real_flags < 0)
         return _EINVAL;
