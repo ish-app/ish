eb628d2fc33ebf93fe204e311e69e6191d938dc0
Theodore Dubois <tblodt@icloud.com>
2019-10-06 14:29:05 -0700

    Make the about screen dark if the terminal is dark
    Fixes #493

---
+++ app/AboutNavigationController.h
@@ -0,0 +1,16 @@
+//
+//  AboutNavigationController.h
+//  iSH
+//
+//  Created by Theodore Dubois on 10/6/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AboutNavigationController : UINavigationController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ app/AboutNavigationController.m
@@ -0,0 +1,39 @@
+//
+//  AboutNavigationController.m
+//  iSH
+//
+//  Created by Theodore Dubois on 10/6/19.
+//
+
+#import "AboutNavigationController.h"
+#import "UserPreferences.h"
+
+@interface AboutNavigationController ()
+
+@end
+
+@implementation AboutNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [[UserPreferences shared] addObserver:self forKeyPath:@"theme" options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionInitial context:nil];
+}
+
+- (void)dealloc {
+    [[UserPreferences shared] removeObserver:self forKeyPath:@"theme"];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    if (@available(iOS 13, *)) {
+        if ([keyPath isEqualToString:@"theme"]) {
+            UIKeyboardAppearance appearance = UserPreferences.shared.theme.keyboardAppearance;
+            if (appearance == UIKeyboardAppearanceDark) {
+                self.overrideUserInterfaceStyle = UIUserInterfaceStyleDark;
+            } else {
+                self.overrideUserInterfaceStyle = UIUserInterfaceStyleLight;
+            }
+        }
+    }
+}
+
+@end
