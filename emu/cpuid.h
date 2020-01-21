df2ecc95a8a8b17065b104475a0f1440e341bb52
Theodore Dubois <tblodt@icloud.com>
2020-01-20 17:56:53 -0800

    Add cmov to the cpuid bits
    (#608)

--- emu/cpuid.h
+++ emu/cpuid.h
@@ -17,7 +17,7 @@ static inline void do_cpuid(dword_t *eax, dword_t *ebx, dword_t *ecx, dword_t *e
             *eax = 0x0; // say nothing about cpu model number
             *ebx = 0x0; // processor number 0, flushes 0 bytes on clflush
             *ecx = 0b00000000000000000000000000000000; // we support none of the features in ecx
-            *edx = 0b00000000000000000000000000000000; // we also support none of the features in edx
+            *edx = 0b00000000000000001000000000000000; // just cmov
             break;
     }
 }
