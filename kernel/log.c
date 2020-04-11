19e7bbd8ec6ee9ca6fd37a7ed616581d88ab1b20
Theodore Dubois <tblodt@icloud.com>
2020-04-11 13:54:46 -0700

    Log using writev instead of dprintf
    Makes host strace less noisy.

--- kernel/log.c
+++ kernel/log.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <stdarg.h>
 #include <string.h>
+#include <sys/uio.h>
 #if LOG_HANDLER_NSLOG
 #include <CoreFoundation/CoreFoundation.h>
 #endif
@@ -133,11 +134,8 @@ void printk(const char *msg, ...) {
 #if LOG_HANDLER_DPRINTF
 #define NEWLINE "\r\n"
 static void log_line(const char *line) {
-    // glibc has a bug where dprintf to an invalid file descriptor leaks memory
-    // https://sourceware.org/bugzilla/show_bug.cgi?id=22876
-    if (fcntl(666, F_GETFL) == -1 && errno == EBADF)
-        return;
-    dprintf(666, "%s" NEWLINE, line);
+    struct iovec output[2] = {{(void *) line, strlen(line)}, {"\n", 1}};
+    writev(666, output, 2);
 }
 #elif LOG_HANDLER_NSLOG
 static void log_line(const char *line) {
