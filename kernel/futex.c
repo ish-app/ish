f7ecc41f65770e4e22ae62fc952d26d7af29d556
Theodore Dubois <tblodt@icloud.com>
2020-06-25 00:17:22 -0700

    Implement FUTEX_REQUEUE
    Fixes #472 and node hanging on exit (#90)

--- kernel/futex.c
+++ kernel/futex.c
@@ -1,11 +1,23 @@
 #include "kernel/calls.h"
 
+#define FUTEX_WAIT_ 0
+#define FUTEX_WAKE_ 1
+#define FUTEX_REQUEUE_ 3
+#define FUTEX_PRIVATE_FLAG_ 128
+#define FUTEX_CMD_MASK_ ~(FUTEX_PRIVATE_FLAG_)
+
 struct futex {
     atomic_uint refcount;
     struct mem *mem;
     addr_t addr;
-    cond_t cond;
+    struct list queue;
     struct list chain; // locked by futex_hash_lock
+};
+
+struct futex_wait {
+    cond_t cond;
+    struct futex *futex; // will be changed by a requeue
+    struct list queue;
 };
 
 #define FUTEX_HASH_BITS 12
@@ -18,10 +30,8 @@ static void __attribute__((constructor)) init_futex_hash() {
         list_init(&futex_hash[i]);
 }
 
-// returns the futex for the current process at the given addr, and locks it
-static struct futex *futex_get(addr_t addr) {
+static struct futex *futex_get_unlocked(addr_t addr) {
     int hash = (addr ^ (unsigned long) current->mem) % FUTEX_HASH_SIZE;
-    lock(&futex_lock);
     struct list *bucket = &futex_hash[hash];
     struct futex *futex;
     list_for_each_entry(bucket, futex, chain) {
@@ -39,18 +49,33 @@ static struct futex *futex_get(addr_t addr) {
     futex->refcount = 1;
     futex->mem = current->mem;
     futex->addr = addr;
-    cond_init(&futex->cond);
+    list_init(&futex->queue);
     list_add(bucket, &futex->chain);
     return futex;
 }
 
-// must be called on the result of futex_get when you're done with it
-static void futex_put(struct futex *futex) {
+// Returns the futex for the current process at the given addr, and locks it
+// Unlocked variant is available for times when you need to get two futexes at once
+static struct futex *futex_get(addr_t addr) {
+    lock(&futex_lock);
+    struct futex *futex = futex_get_unlocked(addr);
+    if (futex == NULL)
+        unlock(&futex_lock);
+    return futex;
+}
+
+static void futex_put_unlocked(struct futex *futex) {
     if (--futex->refcount == 0) {
-        cond_destroy(&futex->cond);
+        assert(list_empty(&futex->queue));
         list_remove(&futex->chain);
         free(futex);
     }
+}
+
+// Must be called on the result of futex_get when you're done with it
+// Also has an unlocked version, for releasing the result of futex_get_unlocked
+static void futex_put(struct futex *futex) {
+    futex_put_unlocked(futex);
     unlock(&futex_lock);
 }
 
@@ -65,7 +90,7 @@ static int futex_load(struct futex *futex, dword_t *out) {
     return 0;
 }
 
-int futex_wait(addr_t uaddr, dword_t val, struct timespec *timeout) {
+static int futex_wait(addr_t uaddr, dword_t val, struct timespec *timeout) {
     struct futex *futex = futex_get(uaddr);
     int err = 0;
     dword_t tmp;
@@ -73,29 +98,58 @@ int futex_wait(addr_t uaddr, dword_t val, struct timespec *timeout) {
         err = _EFAULT;
     else if (tmp != val)
         err = _EAGAIN;
-    else
-        err = wait_for(&futex->cond, &futex_lock, timeout);
+    else {
+        struct futex_wait wait;
+        wait.cond = COND_INITIALIZER;
+        wait.futex = futex;
+        list_add_tail(&futex->queue, &wait.queue);
+        err = wait_for(&wait.cond, &futex_lock, timeout);
+        futex = wait.futex;
+        list_remove_safe(&wait.queue);
+    }
     futex_put(futex);
     STRACE("%d end futex(FUTEX_WAIT)", current->pid);
     return err;
 }
 
-int futex_wake(addr_t uaddr, dword_t val) {
+static int futex_wakelike(int op, addr_t uaddr, dword_t wake_max, dword_t requeue_max, addr_t requeue_addr) {
     struct futex *futex = futex_get(uaddr);
-    if (val == 1)
-        notify_once(&futex->cond);
-    else if (val == 0x7fffffff)
-        notify(&futex->cond);
-    else
-        TODO("invalid number of futex wakes %d", val);
+
+    struct futex_wait *wait, *tmp;
+    unsigned woken = 0;
+    list_for_each_entry_safe(&futex->queue, wait, tmp, queue) {
+        if (woken >= wake_max)
+            break;
+        notify(&wait->cond);
+        list_remove(&wait->queue);
+        woken++;
+    }
+
+    if (op == FUTEX_REQUEUE_) {
+        struct futex *futex2 = futex_get_unlocked(requeue_addr);
+        unsigned requeued = 0;
+        list_for_each_entry_safe(&futex->queue, wait, tmp, queue) {
+            if (requeued >= requeue_max)
+                break;
+            // sketchy as hell
+            list_remove(&wait->queue);
+            list_add_tail(&futex2->queue, &wait->queue);
+            assert(futex->refcount > 1); // should be true because this function keeps a reference
+            futex->refcount--;
+            futex2->refcount++;
+            requeued++;
+        }
+        futex_put_unlocked(futex2);
+        woken += requeued;
+    }
+
     futex_put(futex);
-    return val; // FIXME wrong if val is INT_MAX
+    return woken;
 }
 
-#define FUTEX_WAIT_ 0
-#define FUTEX_WAKE_ 1
-#define FUTEX_PRIVATE_FLAG_ 128
-#define FUTEX_CMD_MASK_ ~(FUTEX_PRIVATE_FLAG_)
+int futex_wake(addr_t uaddr, dword_t wake_max) {
+    return futex_wakelike(FUTEX_WAKE_, uaddr, wake_max, 0, 0);
+}
 
 dword_t sys_futex(addr_t uaddr, dword_t op, dword_t val, addr_t timeout_or_val2, addr_t uaddr2, dword_t val3) {
     if (!(op & FUTEX_PRIVATE_FLAG_)) {
@@ -115,7 +169,10 @@ dword_t sys_futex(addr_t uaddr, dword_t op, dword_t val, addr_t timeout_or_val2,
             return futex_wait(uaddr, val, timeout_or_val2 ? &timeout : NULL);
         case FUTEX_WAKE_:
             STRACE("futex(FUTEX_WAKE, %#x, %d)", uaddr, val);
-            return futex_wake(uaddr, val);
+            return futex_wakelike(op & FUTEX_CMD_MASK_, uaddr, val, 0, 0);
+        case FUTEX_REQUEUE_:
+            STRACE("futex(FUTEX_REQUEUE, %#x, %d, %#x)", uaddr, val, uaddr2);
+            return futex_wakelike(op & FUTEX_CMD_MASK_, uaddr, val, timeout_or_val2, uaddr2);
     }
     STRACE("futex(%#x, %d, %d, timeout=%#x, %#x, %d) ", uaddr, op, val, timeout_or_val2, uaddr2, val3);
     FIXME("unsupported futex operation %d", op);
