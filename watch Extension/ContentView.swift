9af3eb90aeccf024badd073e0494d57ac9e74b8f
Alex Gustafsson <alex-gustafsson@live.se>
2020-07-26 11:31:59 +0200

    Add watch project

---
+++ watch Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  watch Extension
+//
+//  Created by Alex Gustafsson on 2020-07-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ watch Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  watch Extension
+//
+//  Created by Alex Gustafsson on 2020-07-26.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
