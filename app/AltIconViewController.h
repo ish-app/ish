d1a10bd26dd98b33a601caf714e1518c58b63a93
Theodore Dubois <tblodt@icloud.com>
2020-08-17 23:15:55 -0700

    Reland "Add a menu where you can choose the app icon"
    Last attempt didn't work out because it inadvertently removed the
    ASSETCATALOG_COMPILER_APPICON_NAME from the xcodeproj.
    
    This reverts commit c65cc4bfa78382884a3a02c1b4391597a71a0568.

---
+++ app/AltIconViewController.h
@@ -0,0 +1,16 @@
+//
+//  IconViewController.h
+//  iSH
+//
+//  Created by Theodore Dubois on 12/13/19.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface AltIconViewController : UIViewController <UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout>
+
+@end
+
+NS_ASSUME_NONNULL_END
