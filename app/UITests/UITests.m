dec9e9c3bf0b63fa2f399c20bab83e959a58bf42
Theodore Dubois <tblodt@icloud.com>
2020-11-13 20:59:14 -0800

    Add empty UI tests target

---
+++ app/UITests/UITests.m
@@ -0,0 +1,19 @@
+//
+//  UITests.m
+//  UITests
+//
+//  Created by Theodore Dubois on 11/13/20.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface UITests : XCTestCase
+@end
+
+@implementation UITests
+
+- (void)setUp {
+    self.continueAfterFailure = NO;
+}
+
+@end
