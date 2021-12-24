530088de1c3fcb6ef6c49c7626fc0f8b38aa4697
Theodore Dubois <tblodt@icloud.com>
2021-12-24 15:50:21 -0800

    Check out the correct submodule commit in clone-linux.sh
    Also removes the blob filter, because for some reason this makes git submodule
    update want to download the full repository history just to check if the
    requested commit exists.

--- deps/clone-linux.sh
+++ deps/clone-linux.sh
@@ -3,6 +3,7 @@ repo_root="$(dirname $0)/.."
 sub_path=deps/linux
 sub_repo="$repo_root/$sub_path"
 git submodule init "$sub_path"
-git clone "$(git config submodule.$sub_path.url)" --depth=1 --sparse --filter=blob:none "$sub_repo"
+git clone "$(git config submodule."$sub_path".url)" --depth=1 --sparse "$sub_repo"
 git -C "$sub_repo" sparse-checkout set --stdin < "$sub_repo/../linux-sparse.txt"
-git config --local submodule.$sub_path.update checkout
+git config --local submodule."$sub_path".update checkout
+git submodule update --depth=1 "$sub_path"
