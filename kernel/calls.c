91a438c6647c1ba1ca04fd8c0ea9693d3957284a
Mike Miller <emkey1@gmail.com>
2021-12-26 14:28:15 -0800

    Update kernel/calls.c
    Co-authored-by: Saagar Jha <saagar@saagarjha.com>

--- kernel/calls.c
+++ kernel/calls.c
@@ -254,7 +254,6 @@ void handle_interrupt(int interrupt) {
         } else {
             if (syscall_table[syscall_num] == (syscall_t) syscall_stub) {
                 printk("%d(%s) stub syscall %d\n",current->pid, current->comm, syscall_num);
-
             }
             STRACE("%d call %-3d ", current->pid, syscall_num);
             int result = syscall_table[syscall_num](cpu->ebx, cpu->ecx, cpu->edx, cpu->esi, cpu->edi, cpu->ebp);
