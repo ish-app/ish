c5561d2fbc34f04fd224f24bdb653b8664a794b9
Mike Miller <emkey1@oneopa.com>
2021-12-27 07:18:14 -0800

    Modify get_uptime() to work properly with non busybox calls to uptime().  This breaks the busybox uptime, but fixes everything else so far as I can tell.

--- platform/darwin.c
+++ platform/darwin.c
@@ -1,10 +1,13 @@
 #include <mach/mach.h>
+#include <mach/mach_time.h>
 #include <sys/sysctl.h>
 #include <sys/time.h>
 #include <sys/mman.h>
 #include "kernel/errno.h"
 #include "platform/platform.h"
 #include "debug.h"
+
+typedef double CFTimeInterval;
 
 struct cpu_usage get_total_cpu_usage() {
     host_cpu_load_info_data_t load;
@@ -39,6 +42,23 @@ struct mem_usage get_mem_usage() {
     return usage;
 }
 
+CFTimeInterval getSystemUptime(void)
+{
+    enum { NANOSECONDS_IN_SEC = 1000 * 1000 * 1000 };
+    static double multiply = 0;
+    if (multiply == 0)
+    {
+        mach_timebase_info_data_t s_timebase_info;
+        kern_return_t result = mach_timebase_info(&s_timebase_info);
+        assert(result == 0);
+        // multiply to get value in the nano seconds
+        multiply = (double)s_timebase_info.numer / (double)s_timebase_info.denom;
+        // multiply to get value in the seconds
+        multiply /= NANOSECONDS_IN_SEC;
+    }
+    return mach_continuous_time() * multiply;
+}
+
 struct uptime_info get_uptime() {
     uint64_t kern_boottime[2];
     size_t size = sizeof(kern_boottime);
@@ -62,7 +82,8 @@ struct uptime_info get_uptime() {
     }
 
     struct uptime_info uptime = {
-        .uptime_ticks = now.tv_sec - kern_boottime[0],
+        //.uptime_ticks = now.tv_sec - kern_boottime[0],
+        .uptime_ticks = getSystemUptime() * 100, // This works but shouldn't.  -mke
         .load_1m = vm_loadavg.ldavg[0],
         .load_5m = vm_loadavg.ldavg[1],
         .load_15m = vm_loadavg.ldavg[2],
