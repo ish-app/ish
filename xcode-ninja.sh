e7ec5194717ca4697596178a513ab881bc15b89d
Theodore Dubois <tblodt@icloud.com>
2022-08-23 08:13:33 -0700

    Use printenv for path detection

--- xcode-ninja.sh
+++ xcode-ninja.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Try to figure out the user's PATH to pick up their installed utilities.
-export PATH="$PATH:$(sudo -u "$USER" -i echo '$PATH')"
+export PATH="$PATH:$(sudo -u "$USER" -i printenv PATH)"
 
 ninja "$@"
