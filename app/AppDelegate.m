ad07a7a56d61bbc78c14bd30df6b8b3008090165
Saagar Jha <saagar@saagarjha.com>
2023-09-20 19:59:28 -0700

    Use AF_INET for network reachability callback

--- app/AppDelegate.m
+++ app/AppDelegate.m
@@ -297,9 +297,10 @@ void NetworkReachabilityCallback(SCNetworkReachabilityRef target, SCNetworkReach
         });
     }];
     
-    struct sockaddr_in6 address = {
-        .sin6_len = sizeof(address),
-        .sin6_family = AF_INET6,
+    // This code is IPv4 and IPv6 aware: see https://developer.apple.com/library/archive/samplecode/Reachability/Listings/ReadMe_md.html.
+    struct sockaddr_in address = {
+        .sin_len = sizeof(address),
+        .sin_family = AF_INET,
     };
     self.reachability = SCNetworkReachabilityCreateWithAddress(kCFAllocatorDefault, (struct sockaddr *) &address);
     SCNetworkReachabilityContext context = {
